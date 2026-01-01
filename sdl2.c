a1e55cda5028f3fff41d650413ce7f37ff0cce7d
hwtwcx <2169859520@qq.com>
2026-01-01 22:50:13 +0800

    sdl2 update

--- sdl2.c
+++ sdl2.c
@@ -2,6 +2,7 @@
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_pixels.h>
 #include <SDL2/SDL_render.h>
+#include <SDL2/SDL_stdinc.h>
 #include <SDL2/SDL_video.h>
 #include "bmp.h"
 int main(int argc, char *argv[])
@@ -12,8 +13,32 @@ int main(int argc, char *argv[])
 	SDL_Event event;
 	load("/home/hwt/out.bmp", &pixcels, &header,&rowsize);
 	SDL_Window* window=SDL_CreateWindow("test pixcels", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, header.width, header.height, SDL_WINDOW_SHOWN);
-	SDL_Renderer* render=SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	SDL_Texture*texture=SDL_CreateTexture(render,SDL_PIXELFORMAT_ABGR8888 , SDL_TEXTUREACCESS_STREAMING, header.width, header.height);
+	SDL_Renderer* renderer=SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	SDL_Texture*texture=SDL_CreateTexture(renderer,SDL_PIXELFORMAT_ABGR8888 , SDL_TEXTUREACCESS_STREAMING, header.width, header.height);
+	Uint32 texture_format;/*start getting SDL_PixelFormat*/
+	int ___=SDL_PIXELFORMAT_ABGR8888;
+	SDL_QueryTexture(texture, &texture_format, &___,&header.width, &header.height);
+	SDL_PixelFormat *____=SDL_AllocFormat(texture_format);/*end*/
+	void *_;
+	int pitch;
+	SDL_LockTexture(texture,NULL, &_, &pitch);/*copy pixcels to texture*/
+
+	Uint32* pixcels_=(Uint32*)_;
+	for (int y; y<header.height; y++) {
+		for (int x; x<header.width; x++) {
+			struct pixcel __=pixcels[y][x];
+			pixcels_[y * (pitch / 4) + x]=SDL_MapRGB(____, __.red, __.green, __.blue);
+		}
+	}
+	SDL_UnlockTexture(texture);/*end*/
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+        
+        // 渲染纹理
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+        
+        // 更新屏幕
+    SDL_RenderPresent(renderer);
 	free_pixcels(&pixcels,header.height);
 	while (1) {
 		SDL_PollEvent(&event);
@@ -22,7 +47,7 @@ int main(int argc, char *argv[])
 		}
 	}
 	SDL_DestroyTexture(texture);
-	SDL_DestroyRenderer(render);
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return EXIT_SUCCESS;
